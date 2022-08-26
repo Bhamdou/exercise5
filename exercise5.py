8c9859d842ee62902aca803de37b6c804ef11c5f
Bhamdou <qgubb21@gmail.com>
2022-08-26 08:47:08 +0200

    first commit

---
+++ exercise5.py
@@ -0,0 +1,12 @@
+# user=input("type your phrase:")
+
+# vowel_list=["a","e","i","o","u"]
+
+# def inator(user):
+#     if user[-1] in vowel_list:      
+#         result = name + "inator"
+#         print(f"{user} - inator{str(len(user))}000")
+#     else:
+#      print(user + "inator" + str(len(user))+"000")
+
+# inator(user)
